bc373d4622d9ac4340d5efb39d09141e4715fd7e
Hacktin <isues41@gmail.com>
2020-09-16 12:06:01 +0300

    if else statment has been center

---
+++ Core/Business/BusinessLogicRunner.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Policy;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace forgeSampleAPI_DotNetCore.Core.Business
+{
+    public static class BusinessLogicRunner
+    {
+
+        public static T RunnerStatmentOptional<T>(bool statment, T t1, T t2) where T:class
+        {
+            if (statment)
+            {
+                return t1;
+            }
+
+            else
+            {
+                return t2;
+            }
+        }
+        
+
+    }
+}
